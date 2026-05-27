8bdd2b7c60088cc895bfcf910b4fe9e32e70420d
EC2 Default User <ec2-user@ip-172-16-63-121.us-east-2.compute.internal>
2026-05-27 10:35:52 +0000

    add skyrl patches

--- scripts/install_sky.sh
+++ scripts/install_sky.sh
@@ -1,7 +1,17 @@
-git clone https://github.com/novasky-ai/SkyRL.git
-uv venv --python 3.12 sky --seed
-source sky/bin/activate
-cd SkyRL/skyrl-train
+#!/bin/bash
+set -e
 
-uv sync --active --extra vllm
-uv pip install deepspeed datasets
+# Run from: endless-terminals-playground/
+
+python3.12 -m venv /tmp/sky
+source /tmp/sky/bin/activate
+
+pip install -e SkyRL[fsdp,vllm]
+pip install "ray[default]==2.51.1"
+pip install -e .
+
+cd SkyRL
+git apply ../scripts/skyrl_patches.patch
+cd ..
+
+echo "Done. Activate with: source /tmp/sky/bin/activate"
